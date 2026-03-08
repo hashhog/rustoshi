f142f0237d57ad18cd30cf0113a363a150da0f89
Max <maxtclinton@gmail.com>
2026-03-08 08:58:52 -0400

    add consensus crate

---
+++ crates/consensus/src/lib.rs
@@ -0,0 +1,11 @@
+//! Rustoshi consensus crate
+//!
+//! Bitcoin consensus rules: block validation, chain selection, difficulty adjustment.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+}
