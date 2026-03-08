a9dfb20cfed5703815f038ae0353eff818808706
max <maxtclinton@gmail.com>
2026-03-08 08:58:43 -0400

    add primitives crate

---
+++ crates/primitives/src/lib.rs
@@ -0,0 +1,11 @@
+//! Rustoshi primitives crate
+//!
+//! Foundational types used throughout the rustoshi node.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+}
