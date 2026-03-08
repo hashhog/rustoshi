34e92bf624b8f8b2efdea9a241912469f3f2ad72
max <maxtclinton@gmail.com>
2026-03-08 08:59:03 -0400

    add storage crate

---
+++ crates/storage/src/lib.rs
@@ -0,0 +1,11 @@
+//! Rustoshi storage crate
+//!
+//! Persistent storage for blocks, transactions, and chain state.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+}
