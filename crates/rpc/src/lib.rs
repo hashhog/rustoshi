309de4240376cb65dcfb12dc6d65d2dc0e3bfea1
max <maxtclinton@gmail.com>
2026-03-08 08:59:05 -0400

    add rpc crate

---
+++ crates/rpc/src/lib.rs
@@ -0,0 +1,11 @@
+//! Rustoshi rpc crate
+//!
+//! JSON-RPC server for node interaction and wallet operations.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+}
