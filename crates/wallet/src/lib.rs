7bef4c60e5167f7d20be6b6da4a96e18fed8cc58
max <maxtclinton@gmail.com>
2026-03-08 08:59:07 -0400

    add wallet crate

---
+++ crates/wallet/src/lib.rs
@@ -0,0 +1,11 @@
+//! Rustoshi wallet crate
+//!
+//! Key management, address generation, and transaction signing.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+}
