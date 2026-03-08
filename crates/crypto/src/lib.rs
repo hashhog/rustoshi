58ade4e8eace328befc2ccae051b4c83d20b8aa2
Max <maxtclinton@gmail.com>
2026-03-08 08:58:49 -0400

    add crypto crate

---
+++ crates/crypto/src/lib.rs
@@ -0,0 +1,11 @@
+//! Rustoshi crypto crate
+//!
+//! Cryptographic primitives for Bitcoin: hashing, signing, verification.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+}
