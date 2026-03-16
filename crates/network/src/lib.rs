705933aa1d529a2862f4a9baeb13da9fe789f265
Max <maxtclinton@gmail.com>
2026-03-15 21:33:08 -0400

    export relay module from network crate

--- crates/network/src/lib.rs
+++ crates/network/src/lib.rs
@@ -4,12 +4,18 @@
 
 pub mod block_download;
 pub mod header_sync;
+pub mod headers_presync;
 pub mod message;
+pub mod misbehavior;
 pub mod peer;
 pub mod peer_manager;
+pub mod relay;
 
 pub use block_download::*;
 pub use header_sync::*;
+pub use headers_presync::*;
 pub use message::*;
+pub use misbehavior::*;
 pub use peer::*;
 pub use peer_manager::*;
+pub use relay::*;
